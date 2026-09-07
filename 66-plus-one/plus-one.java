9173a3f5ae6a04c8034196829368cafd7924e46d
Sangmesh Veershette <sangameshveershette@gmail.com>
2026-09-07 18:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (90.26%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -3,12 +3,14 @@ class Solution {
         for(int i=digits.length-1;i>=0;i=i-1){
             if(digits[i]==9){
                 digits[i]=0;
-            }else{
+            }
+            else{
                 digits[i]=digits[i]+1;
                 return digits;
             }
         }
-        int res[] = new int[digits.length+1];
-        res[0]=1;return res;
+        int res[]=new int[digits.length+1];
+        res[0]=1;
+        return res;
     }
 }
