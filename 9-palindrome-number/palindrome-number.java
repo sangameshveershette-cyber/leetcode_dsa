40761bb034706668b2f2f9eeede84cb998901dfe
Sangmesh Veershette <sangameshveershette@gmail.com>
2026-09-08 22:40:15 +0530

    Time: 5 ms (82.40%) | Memory: 46.1 MB (16.16%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -8,7 +8,7 @@ class Solution {
         while(n>0){
             int d = n%10;
             rev = rev*10+d;
-            n = n/10;
+            n=n/10;
         }
         if(rev==x){
             return true;
